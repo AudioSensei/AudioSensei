eb386eb02d5d323c68df91f0bba1fdba367b65b4
Cossinus <44206036+Cossinus@users.noreply.github.com>
2020-10-02 15:43:44 +0200

    Configure Serilog (#8)

--- AudioSensei/App.xaml.cs
+++ AudioSensei/App.xaml.cs
@@ -2,8 +2,11 @@
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
+using Avalonia.Logging.Serilog;
 using AudioSensei.ViewModels;
 using AudioSensei.Views;
+using Serilog;
+using System.IO;
 
 namespace AudioSensei
 {
@@ -11,6 +14,25 @@ namespace AudioSensei
     {
         public override void Initialize()
         {
+            const string template = "[{Timestamp:yyyy-MM-dd HH:mm:ss}] [{Level:u3}] {Message:lj}{NewLine}{Exception}";
+            const string latestLogPath = @"logs\latest.log";
+
+            if (File.Exists(latestLogPath))
+            {
+                File.Delete(latestLogPath);
+            }
+
+            var logger = new LoggerConfiguration()
+                .MinimumLevel.Information()
+                .WriteTo.File(latestLogPath, outputTemplate: template)
+                .WriteTo.File(@"logs\log-.log", outputTemplate: template, rollingInterval: RollingInterval.Day, retainedFileCountLimit: 9)
+                .CreateLogger();
+
+            SerilogLogger.Initialize(logger);
+            Log.Logger = logger;
+
+            Log.Information("Opening a new instance of AudioSensei");
+
             AvaloniaXamlLoader.Load(this);
         }
 
