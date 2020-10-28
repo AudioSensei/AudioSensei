7b84d6d071867f0d66a63d9a944786cbdf2fca06
petris <paprikapolishgamer@gmail.com>
2020-10-28 23:48:55 +0100

    Add discord rpc

---
+++ AudioSensei/Discord/DiscordPartyPrivacy.cs
@@ -0,0 +1,8 @@
+﻿namespace AudioSensei.Discord
+{
+    internal enum DiscordPartyPrivacy
+    {
+        Private = 0,
+        Public = 1
+    }
+}
---
+++ AudioSensei/Discord/DiscordUserData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace AudioSensei.Discord
+{
+    [StructLayout(LayoutKind.Sequential)]
+    internal struct DiscordUserData
+    {
+        public IntPtr userId;
+        public IntPtr username;
+        public IntPtr discriminator;
+        public IntPtr avatar;
+    }
+}
