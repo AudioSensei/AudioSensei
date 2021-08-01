9acb59f20fa27c11ac5542831b6352cbf178ef5a
petris <paprikapolishgamer@gmail.com>
2021-08-01 14:59:55 +0200

    Add playback server

---
+++ AudioSensei/Crypto/Crc32C/Crc32CSse32.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Runtime.Intrinsics.X86;
+
+namespace AudioSensei.Crypto.Crc32C
+{
+    internal sealed class Crc32CSse32 : Crc32C
+    {
+        public static bool Supported => Sse42.IsSupported;
+
+        internal Crc32CSse32()
+        {
+            if (!Supported)
+            {
+                throw new PlatformNotSupportedException();
+            }
+        }
+
+        protected override uint Append(uint crc, ReadOnlySpan<byte> data)
+        {
+            int processed = 0;
+            if (data.Length > sizeof(uint))
+            {
+                processed = data.Length / sizeof(uint) * sizeof(uint);
+                ReadOnlySpan<uint> uints = MemoryMarshal.Cast<byte, uint>(data.Slice(0, processed));
+                for (int i = 0; i < uints.Length; i++)
+                {
+                    crc = Sse42.Crc32(crc, uints[i]);
+                }
+            }
+
+            for (int i = processed; i < data.Length; i++)
+            {
+                crc = Sse42.Crc32(crc, data[i]);
+            }
+
+            return crc;
+        }
+    }
+}
